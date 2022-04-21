949370478153ad712db0ce33d6c9716e8a606bc3
jamesworden <jamesworden.ny@gmail.com>
2022-04-20 22:00:45 -0400

    refactor date utils function name

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -2,8 +2,8 @@ export class DateUtils {
   static daysBetween(startDate: Date, endDate: Date) {
     const millisecondsPerDay = 24 * 60 * 60 * 1000
 
-    const endDateInMs = this.treatAsUTC(endDate).getTime()
-    const startDateInMs = this.treatAsUTC(startDate).getTime()
+    const endDateInMs = this.getDateAsUTC(endDate).getTime()
+    const startDateInMs = this.getDateAsUTC(startDate).getTime()
     const timeDiffInMs = endDateInMs - startDateInMs
     const daysAsDecimal = timeDiffInMs / millisecondsPerDay
     const days = Math.floor(daysAsDecimal)
@@ -11,7 +11,7 @@ export class DateUtils {
     return days
   }
 
-  static treatAsUTC(date: Date) {
+  private static getDateAsUTC(date: Date) {
     var result = new Date(date)
     result.setMinutes(result.getMinutes() - result.getTimezoneOffset())
     return result
